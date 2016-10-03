7b41e6bed7680ab5706602a93cbb7142a893e577
Kiril Vatev <vatev.1@gmail.com>
2016-10-03 18:23:14 -0400

    adding gulp build that can generate minified files with sourcemaps

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('fixtures', function () {
+  return gulp.src('fixtures/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify({
+      mangle: true
+    }))
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.js'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('bin'));
+});
