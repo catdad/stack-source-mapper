5d5c0460236d6951864ebfbc7c672673f5359b3e
Kiril Vatev <vatev.1@gmail.com>
2016-10-06 16:47:02 -0400

    added gulp tasks to generate stack trace files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,42 @@
 /* jshint node: true */
 
+var path = require('path');
+var fs = require('fs');
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var shellton = require('shellton');
+var del = require('del');
+
+function getStackTrace(file, done) {
+  shellton('node ' + file, function (err, stdout, stderr) {
+    stderr = stderr.replace(/\r\n/g, '\n');
+
+    var stack = stderr.split('\n').slice(4).join('\n');
+
+    done(null, stack);
+  });
+}
+
+gulp.task('stack:minified', function (done) {
+  var file = path.join('bin', 'error.min.js');
+
+  getStackTrace(file, function (err, stack) {
+    fs.writeFile(path.resolve('bin', 'error.err'), stack, done);
+  });
+});
+
+gulp.task('stack:source', function (done) {
+  var file = path.join('fixtures', 'error.js');
+
+  getStackTrace(file, function (err, stack) {
+    fs.writeFile(path.resolve('bin', 'error.control.err'), stack, done);
+  });
+});
+
+gulp.task('stack', ['stack:source', 'stack:minified']);
 
 gulp.task('fixtures', function () {
   return gulp.src('fixtures/*.js')
@@ -18,4 +51,8 @@ gulp.task('fixtures', function () {
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('bin'));
+});
+
+gulp.task('clean', function () {
+  return del('bin');
 });
