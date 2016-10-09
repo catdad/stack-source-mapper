96ac331327a0aa86d50661547c080d5558615157
Kiril Vatev <vatev.1@gmail.com>
2016-10-09 17:35:40 -0400

    cleanup

--- test.js
+++ test.js
@@ -6,14 +6,10 @@ var path = require('path');
 var errFile = fs.readFileSync(path.resolve('bin', 'error.err')).toString().trim();
 var mapFile = JSON.parse(fs.readFileSync(path.resolve('bin', 'error.min.js.map')).toString());
 
-var filePath = 'error.min.js';
-
 var mapper = require('./');
 
-var result = mapper(errFile, {
+console.log(mapper(errFile, {
   '**/error.min.js': mapFile
 }, {
   root: process.cwd()
-});
-
-console.log(result);
+}));
