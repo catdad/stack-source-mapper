e4e31ddb4f8e77570b60845798a6df736fff31b2
Kiril Vatev <vatev.1@gmail.com>
2016-10-15 14:10:42 -0400

    stubbing out some tests

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+/* jshint node: true, mocha: true */
+
+var expect = require('chai').expect;
+
+describe('[index]', function () {
+  it('transforms a stacktrace using the source map');
+
+  it('throws if stacktrace is not a string');
+  it('throws if sourcemap is not an object');
+});
